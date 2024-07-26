3b4d8a6e013259ef8e5b7e695929a4d437427bee
Turing-26 <mail2sarim@gmail.com>
2024-07-27 04:14:02 +0500

    .

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -3,13 +3,7 @@ import styles from "./BackgroundVideo.module.css";
 const BackgroundVideo = () => {
   return (
     <div className={styles.videoContainer}>
-      <video
-        src="/video1.mp4" // Path to the video in the public folder
-        autoPlay
-        loop
-        muted
-        className={styles.video}
-      />
+      <video src="/video1.mp4" autoPlay loop muted className={styles.video} />
     </div>
   );
 };
