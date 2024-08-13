e8d1fbf778d22cfb166efcde1230893e10132980
Turing-26 <mail2sarim@gmail.com>
2024-08-13 17:15:03 +0500

    .

--- src/components/Reviews/ReviewSlider.jsx
+++ src/components/Reviews/ReviewSlider.jsx
@@ -1,24 +1,19 @@
 "use client";
-import React, { useRef, useState, useLayoutEffect } from "react";
+import React, { useRef, useState, useLayoutEffect, useEffect } from "react";
 import {
-  Carousel,
-  CarouselContentContainer,
-  CarouselContentItem,
   CarouselDetails,
   CarouselSignatures,
   CarouselName,
-  CarouselControls,
-  CarouselBtn,
   CarouselDate,
-  CarouselDot,
 } from "../../components/mui/AboutPkgs";
+import {
+  Carousel,
+  CarouselContentItem,
+  CarouselContentContainer,
+} from "../mui/HomePkgs";
 
 import { Box, makeStyles, styled } from "@mui/material";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
 import MobileStepper from "@mui/material/MobileStepper";
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const testimonials = [
   {
@@ -30,7 +25,8 @@ const testimonials = [
   },
   {
     name: "Jane Smith",
-    feedback: "I loved it. The quality is top-notch and the support is fantastic.",
+    feedback:
+      "I loved it. The quality is top-notch and the support is fantastic.",
     image: "https://swiperjs.com/demos/images/nature-2.jpg",
     date: "30/01/24",
   },
@@ -65,16 +61,66 @@ const StyledMobileStepper = styled(MobileStepper)(({ theme }) => ({
   display: "flex",
   justifyContent: "flex-end",
   ".MuiMobileStepper-dot": {
-    backgroundColor: theme.palette.grey[400], // Default color for dots
+    backgroundColor: theme.palette.grey[400],
   },
   ".MuiMobileStepper-dotActive": {
-    backgroundColor: theme.palette.primary.accent, // Color for active dot
+    backgroundColor: theme.palette.primary.accent,
   },
 }));
 
 export default function ReviewSlider() {
   const maxSteps = testimonials.length;
-  const [activeStep, setActiveStep] = React.useState(0);
+  const sliderRef = useRef(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [activeHeight, setActiveHeight] = useState("auto");
+
+  const transitionStyles = (activeNum) => {
+    const centerNum = 100 * activeNum;
+    const rightNum = centerNum * -1 + 100;
+    const leftNum = centerNum * -1 - 100;
+
+    const styles = {
+      left: { opacity: 0, transform: `translateX(${leftNum}%)` },
+      center: {
+        opacity: 1,
+        transform: `translateX(${centerNum !== 0 ? "-" : ""}${centerNum}%)`,
+      },
+      right: { opacity: 0, transform: `translateX(${rightNum}%)` },
+    };
+
+    return styles;
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setActiveStep((prevActiveStep) =>
+        prevActiveStep === testimonials.length - 1 ? 0 : prevActiveStep + 1
+      );
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [activeStep, testimonials.length]);
+
+  useLayoutEffect(() => {
+    if (sliderRef.current) {
+      const children = sliderRef.current.childNodes;
+      let height = 0;
+
+      children.forEach((el) => {
+        const list = Array.from(el.classList);
+        if (list.includes("active")) {
+          for (let i = 0; i < el.children.length; i++) {
+            // console.log(el.children[i].offsetHeight);
+            height += el.children[i].offsetHeight;
+          }
+          // console.log(height);
+          setActiveHeight(`${height + 100}px`);
+        }
+      });
+    }
+  }, [activeStep]);
 
   const handleStepChange = (step) => {
     setActiveStep(step);
@@ -82,11 +128,61 @@ export default function ReviewSlider() {
 
   return (
     <>
-      <Box sx={{ maxWidth: "50%", flexGrow: 1 }}>
-        <AutoPlaySwipeableViews axis={"x"} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
+      <Box sx={{ width: "50%", flexGrow: 1 }}>
+        <Carousel>
+          <CarouselContentContainer
+            ref={sliderRef}
+            sx={{
+              height: activeHeight,
+              width: `${testimonials.length * 100}%`,
+              alignSelf: "flex-start",
+            }}
+          >
+            {testimonials.map((testimonial, index) => {
+              return (
+                <CarouselContentItem
+                  key={index}
+                  className={`${activeStep === index ? "active" : ""}`}
+                  sx={{
+                    border: "none",
+                    width: `${100 / testimonials.length}%`,
+                    ...(activeStep === 0 && index === testimonials.length - 1
+                      ? transitionStyles(index)["left"]
+                      : activeStep === testimonials.length - 1 && index === 0
+                      ? transitionStyles(index)["right"]
+                      : transitionStyles(index)[
+                          activeStep > index
+                            ? "left"
+                            : activeStep === index
+                            ? "center"
+                            : "right"
+                        ]),
+                  }}
+                >
+                  <CarouselSignatures>
+                    <CarouselDetails>{testimonial.feedback}</CarouselDetails>
+                    <CarouselName>{testimonial.name}</CarouselName>
+                    <CarouselDate>{testimonial.date}</CarouselDate>
+                  </CarouselSignatures>
+                </CarouselContentItem>
+              );
+            })}
+          </CarouselContentContainer>
+        </Carousel>
+
+        {/* <AutoPlaySwipeableViews
+          axis={"x"}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
           {testimonials.map((testimonial, index) => {
             return (
-              <CarouselContentItem key={index} className={`${activeStep === index ? "active" : ""}`} sx={{}}>
+              <CarouselContentItem
+                key={index}
+                className={`${activeStep === index ? "active" : ""}`}
+                sx={{}}
+              >
                 <CarouselSignatures>
                   <CarouselDetails>{testimonial.feedback}</CarouselDetails>
                   <CarouselName>{testimonial.name}</CarouselName>
@@ -95,8 +191,12 @@ export default function ReviewSlider() {
               </CarouselContentItem>
             );
           })}
-        </AutoPlaySwipeableViews>
-        <StyledMobileStepper steps={maxSteps} position="static" activeStep={activeStep} />
+        </AutoPlaySwipeableViews> */}
+        <StyledMobileStepper
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+        />
       </Box>
     </>
   );
