874db6462c9d2454c5647a796a17560a8f08dcab
Faizan Haq <fizoneechan@gmail.com>
2024-08-15 02:34:29 +0500

    .

---
+++ src/lib/data/Autocare.js
@@ -0,0 +1,221 @@
+export const cleanPkgs = {
+  Standard: {
+    types: [
+      {
+        type: "Exterieur",
+        price: {
+          one: "74,95",
+          duur: "45",
+        },
+        pros: ["Exterieur wassen", "Ramen en spiegels reinigen", "Spray wax aanbrengen", "Velgen"],
+        cons: [
+          "❌ Banden zwarten",
+          "❌ Dorpels en deurranden",
+          "❌ Ramen ceramische bescherming aanbrengen",
+          "❌ Kunststofdelen voeden",
+          "❌ Tankklep reinigen",
+        ],
+        extras: {
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen  (Optie: + € 50,-)",
+            "Polijsten chromendelen  (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+          ],
+        },
+      },
+      {
+        type: "Interieur",
+        price: {
+          one: "89,95",
+          duur: "45",
+        },
+        pros: [
+          "Dashboard en vakjes afnemen",
+          "Dorpels en deurranden",
+          "Matten uitstomen",
+          "Ramen en spiegels reinigen",
+          "Stoomreiniging interieur globaal",
+        ],
+        cons: [
+          "❌ Detailing",
+          "❌ Kunststofdelen poetsen",
+          "❌ Leerbehandeling",
+          "❌ Matten stripe",
+          "❌ Stoomreiniging intensief interieur",
+        ],
+        extras: {
+          interior: [
+            "Leerbehandeling  (Optie: + € 50.-)",
+            "Vlekken verwijderen bekleding / Diepte reiniging  (Optie: + € 80,-)",
+            "Honden haren verwijderen (Optie; + €40,-)",
+            "Ozonbehandeling (Optie: + € 95,-)",
+            "Schimmel behandeling  (Optie: + € 105,-)",
+            "Vlekken in het plafond  (Optie: + € 75,-)",
+            "Geur behandeling  (Optie: + € 85,-)",
+          ],
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen  (Optie: + € 50,-)",
+            "Polijsten chromendelen  (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+          ],
+        },
+      },
+      {
+        type: "Combi",
+        price: {
+          one: "139,95",
+          duur: "90",
+        },
+        pros: [
+          " In het ‘Standard Combi Pakket’ krijg je alle vinkjes van het ‘Standard Interieur en Exterieur Pakket’ gecombineerd in één pakket!",
+        ],
+        extras: {
+          interior: [
+            "Leerbehandeling (Optie: + € 50,-)",
+            "Vlekken verwijderen bekleding / diepe reiniging (Optie: + € 80,-)",
+            "Honden haren verwijderen (Optie; + €50,-)",
+            "Ozonbehandeling (Optie: + € 95,-)",
+            "Schimmel behandeling meerdere plekken (Optie: + € 105,-)",
+            "Vlekken in het plafond (Optie: + € 75,-)",
+            "Geurbehandeling (Optie: + € 85,-)",
+          ],
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen (Optie: + € 50,-)",
+            "Polijsten chromendelen (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+            "Glascoating (Optie: op aanvraag)",
+          ],
+        },
+      },
+    ],
+  },
+  Deluxe: {
+    types: [
+      {
+        type: "Exterieur",
+        price: {
+          one: "89,95",
+          duur: "60",
+        },
+        pros: [
+          "Exterieur wassen",
+          "Ramen en spiegels reinigen",
+          "Spray wax aanbrengen",
+          "Velgen",
+
+          "Banden zwarten",
+          "Dorpels en deurranden",
+          "Ramen ceramische bescherming aanbrengen",
+          "Kunststofdelen voeden",
+          "Tankklep reinigen",
+        ],
+        extras: {
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen (Optie: + € 50,-)",
+            "Polijsten chromendelen (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+          ],
+        },
+      },
+      {
+        type: "Combi",
+        price: {
+          one: "189,95",
+          duur: "90/150",
+        },
+        pros: [
+          "Dashboard en vakjes afnemen",
+          "Dorpels en deurranden",
+          "Matten uitstomen",
+          "Ramen en spiegels reinigen",
+          "Vlekken verwijderen / Dieptereiniging",
+          "Detailing",
+          "Kunststofdelen poetsen",
+          "Leerbehandeling",
+          "Matten stripe",
+          "Stoomreiniging intensief interieur",
+        ],
+        extras: {
+          interior: [
+            "Honden haren verwijderen (Optie; + €50,-)",
+            "Ozonbehandeling (Optie: + € 95,-)",
+            "Schimmel behandeling meerdere plekken (Optie: + € 105,-)",
+            "Vlekken in het plafond (Optie: + € 75,-)",
+            "Geurbehandeling (Optie: + € 85,-)",
+          ],
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen (Optie: + € 50,-)",
+            "Polijsten chromendelen (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+          ],
+        },
+      },
+      {
+        type: "Interieur",
+        price: {
+          one: "149,95",
+          duur: "90",
+        },
+        pros: [
+          " In het ‘Deluxe Combi Pakket’ krijg je alle vinkjes van het ‘Deluxe Interieur en Exterieur Pakket’ gecombineerd in één pakket!",
+        ],
+        extras: {
+          interior: [
+            "Honden haren verwijderen (Optie; + €50,-)",
+            "Ozonbehandeling (Optie: + € 95,-)",
+            "Schimmel behandeling  (Optie: + € 105,-)",
+            "Vlekken in het plafond  (Optie: + € 75,-)",
+            "Geur behandeling  (Optie: + € 85,-)",
+          ],
+          exterior: [
+            "Motorkap reiniging  (Optie: + € 50,-)",
+            "Polijsten koplampen (Optie: + € 50,-)",
+            "Polijsten chromendelen (Optie: + € 90,-)",
+          ],
+          detailing: [
+            "Polijsten geheel voertuig in 1 stap – krassen verwijderen (50%) + swirls verwijderen (75%) (Optie: + € 180,-)",
+            "Polijsten geheel voertuig in 2 stappen – krassen verwijderen (80%) + swirls verwijderen (85%) (Optie: + € 295,-)",
+            "Polijsten geheel voertuig in 3 stappen – krassen verwijderen (95%) + swirls verwijderen (95%) (Optie: + € 425,-)",
+            "Lakverzegeling  (Optie: op aanvraag)",
+            "Glascoating  (Optie: op aanvraag)",
+          ],
+        },
+      },
+    ],
+  },
+};
