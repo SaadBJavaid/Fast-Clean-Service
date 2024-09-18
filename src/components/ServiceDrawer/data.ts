acc0c778069ba67117509f1682bffb9ad890c500
Syed Muhammad Hassan Abbas Ali Naqvi <gaha87818@gmail.com>
2024-09-18 10:07:49 -0700

    json/ts file for packages

---
+++ src/components/ServiceDrawer/data.ts
@@ -0,0 +1,181 @@
+
+export const packages = [
+  {
+    "name": "Standard Exterior Package",
+    "packages": [
+      "Exterior washing",
+      "Cleaning windows and mirrors",
+      "Apply spray wax",
+      "Rims"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 36 min.",
+    "price": "€ 74",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Standard Interior Package",
+    "packages": [
+      "Remove dashboard and boxes",
+      "Sills and door edges",
+      "Steaming mats",
+      "Cleaning windows and mirrors",
+      "Vacuuming",
+      "Global interior steam cleaning"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 45 min.",
+    "price": "€ 94",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Standard Combi Package",
+    "packages": [
+      "Blacken tires",
+      "Remove dashboard and boxes",
+      "Sills and door edges",
+      "Exterior washing",
+      "Steaming mats",
+      "Cleaning windows and mirrors",
+      "Vacuuming",
+      "Global interior steam cleaning",
+      "Rims",
+      "Wax"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 101 min.",
+    "price": "€ 149",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Deluxe Exterior Package",
+    "packages": [
+      "Blacken tires",
+      "Sills and door edges",
+      "Exterior washing",
+      "Polishing plastic parts",
+      "Applying ceramic protection to windows",
+      "Cleaning windows and mirrors",
+      "Apply spray wax",
+      "Cleaning the fuel filler cap",
+      "Rims"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 63 min.",
+    "price": "€ 89",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Deluxe Interior Package",
+    "packages": [
+      "Remove dashboard and boxes",
+      "Detailing",
+      "Sills and door edges",
+      "Polishing plastic parts",
+      "Leather treatment",
+      "Mats stripe",
+      "Steaming mats",
+      "Cleaning windows and mirrors",
+      "Vacuuming",
+      "Steam cleaning intensive interior",
+      "Removing stains"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 76 min.",
+    "price": "€ 154",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Deluxe Combi Package",
+    "packages": [
+      "Blacken tires",
+      "Remove dashboard and boxes",
+      "Detailing",
+      "Sills and door edges",
+      "Exterior washing",
+      "Polishing plastic parts",
+      "Leather treatment",
+      "Mats stripe",
+      "Steaming mats",
+      "Ceiling steam cleaning",
+      "Applying ceramic protection to windows",
+      "Cleaning windows and mirrors",
+      "Apply spray wax",
+      "Vacuuming",
+      "Steam cleaning intensive interior",
+      "Cleaning the fuel filler cap",
+      "Rims",
+      "Removing stains"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 134 min.",
+    "price": "€ 189",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Showroom Premium Package",
+    "packages": [
+      "Blacken tires",
+      "Remove dashboard and boxes",
+      "Detailing",
+      "Sills and door edges",
+      "Exterior washing",
+      "Odor treatment",
+      "Leather treatment",
+      "Mats stripe",
+      "Steaming mats",
+      "Engine hood cleaning",
+      "Ceiling steam cleaning",
+      "Polishing chrome parts",
+      "Polish entire vehicle in 1 step - remove scratches (50%) + remove swirls (75%)",
+      "Polishing headlights",
+      "Applying ceramic protection to windows",
+      "Cleaning windows and mirrors",
+      "Apply spray wax",
+      "Vacuuming",
+      "Cleaning the fuel filler cap",
+      "Rims",
+      "Removing stains"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 214 min.",
+    "price": "€ 399",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  },
+  {
+    "name": "Deluxe Interior Package",
+    "packages": [
+      "Remove dashboard and boxes",
+      "Detailing",
+      "Sills and door edges",
+      "Polishing plastic parts",
+      "Leather treatment",
+      "Mats stripe",
+      "Steaming mats",
+      "Cleaning windows and mirrors",
+      "Vacuuming",
+      "Steam cleaning intensive interior",
+      "Removing stains"
+    ],
+    "totalDuration": "N/A",
+    "duration": "± 76 min.",
+    "price": "€ 154",
+    "additionalOptions": [],
+    "durationOptions": [],
+    "cleaningFrequencyOptions": []
+  }
+];
