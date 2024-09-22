89211afa226bc30023bfa88ab19eca0ef7fad25c
Faizan Haq <fizoneechan@gmail.com>
2024-09-22 06:47:19 +0500

    booking packages

---
+++ src/components/mui/BookingFormPackages.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { styled, Box, Typography, Button, ListItem } from "@mui/material";
+
+export const BookingFormHeading = styled(Typography)(({ theme }) => ({
+  fontFamily: "Unbounded",
+  margin: "1rem",
+  padding: "2rem 0",
+  color: "#232E4A",
+  textAlign: "center",
+  fontSize: "2.6rem",
+  fontWeight: "700",
+  lineHeight: "57.6px",
+}));
