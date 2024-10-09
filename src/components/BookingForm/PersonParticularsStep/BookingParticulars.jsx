2c1c0ef04d63d141691b11d47dce663ed1dfcecb
Syed Muhammad Hassan Abbas Ali Naqvi <gaha87818@gmail.com>
2024-10-09 08:52:39 -0700

    Toggle switch updates

--- src/components/BookingForm/PersonParticularsStep/BookingParticulars.jsx
+++ src/components/BookingForm/PersonParticularsStep/BookingParticulars.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Grid, styled } from "@mui/material";
+import { Box, Grid, styled, Checkbox, FormControlLabel, Typography } from "@mui/material";
 import { CustomFormTextField, FormContainer } from "../../../components/mui/NewFormPkgs";
 import { ThemeProvider } from "@emotion/react";
 import { deepmerge } from "@mui/utils";
@@ -10,210 +10,248 @@ import { useValidation } from "../../../contexts/ValidationContext";
 import { useSession } from "next-auth/react";
 
 const BookingParticulars = () => {
-  const form = useMultiStepForm();
-  const { data: session, status } = useSession();
-  const { formData } = form;
-  const { theme } = useTheme();
-  const { updateValidation } = useValidation();
+    const form = useMultiStepForm();
+    const { data: session, status } = useSession();
+    const { formData } = form;
+    const { theme } = useTheme();
+    const { updateValidation } = useValidation();
 
-  const [bookingForm, setBookingForm] = useState({
-    firstName: "",
-    surname: "",
-    companyName: "",
-    street: "",
-    zipCode: "",
-    city: "",
-    email: "",
-    phoneNumber: "",
-    makeModel: "",
-  });
-  console.log(session);
-
-  useEffect(() => {
-    if (session?.user) {
-      setBookingForm({
-        ...bookingForm,
-        email: session.user.email,
-        firstName: session.user.firstName,
-        surname: session.user.lastName,
-        companyName: session.user.companyName,
-        street: session.user.street,
-        city: session.user.city,
-        phoneNumber: session.user.phoneNumber,
-      });
-      form.updateFormData({
-        ...bookingForm,
-        email: session.user.email,
-        firstName: session.user.firstName,
-        surname: session.user.lastName,
-        companyName: session.user.companyName,
-        street: session.user.street,
-        city: session.user.city,
-        phoneNumber: session.user.phoneNumber,
-      });
-    }
-  }, [session?.user, bookingForm, updateValidation, form]);
-
-  useEffect(() => {
-    const isValid = true;
-    updateValidation(isValid);
-  }, [formData, updateValidation]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setBookingForm({
-      ...bookingForm,
-      [name]: value,
+    const [bookingForm, setBookingForm] = useState({
+        firstName: "",
+        surname: "",
+        companyName: "",
+        street: "",
+        zipCode: "",
+        city: "",
+        email: "",
+        phoneNumber: "",
+        makeModel: "",
     });
 
-    form.updateFormData({ [name]: value });
-  };
+    const [isChecked, setIsChecked] = useState(false); // State for the checkbox
 
-  return (
-    <Box sx={{ padding: "2rem 1rem", maxWidth: "800px", margin: "auto" }}>
-      <FormContainer
-        component="form"
-        sx={{ backgroundColor: "transparent", boxShadow: "none" }}
-      >
-        <ThemeProvider theme={(outerTheme) => deepmerge(outerTheme, theme)}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="First Name"
-                name="firstName"
-                value={bookingForm.firstName}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Surname"
-                name="surname"
-                value={bookingForm.surname}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Phone Number"
-                name="phoneNumber"
-                value={bookingForm.phoneNumber}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Company Name"
-                name="companyName"
-                value={bookingForm.companyName}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Street"
-                name="street"
-                value={bookingForm.street}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
+    console.log(session);
 
-            <Grid item xs={6} md={3}>
-              <CustomFormTextField
-                label="City"
-                name="city"
-                value={bookingForm.city}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <CustomFormTextField
-                label="Zip Code"
-                name="zipCode"
-                value={bookingForm.zipCode}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-          </Grid>
+    useEffect(() => {
+        if (session?.user) {
+            setBookingForm({
+                ...bookingForm,
+                email: session.user.email,
+                firstName: session.user.firstName,
+                surname: session.user.lastName,
+                companyName: session.user.companyName,
+                street: session.user.street,
+                city: session.user.city,
+                phoneNumber: session.user.phoneNumber,
+            });
+            form.updateFormData({
+                ...bookingForm,
+                email: session.user.email,
+                firstName: session.user.firstName,
+                surname: session.user.lastName,
+                companyName: session.user.companyName,
+                street: session.user.street,
+                city: session.user.city,
+                phoneNumber: session.user.phoneNumber,
+            });
+        }
+    }, [session?.user, bookingForm, updateValidation, form]);
 
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Email"
-                name="email"
-                value={bookingForm.email}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <CustomFormTextField
-                label="Make and Model of Vehicle"
-                name="makeModel"
-                value={bookingForm.makeModel}
-                onChange={handleChange}
-                fullWidth
-                sx={{
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  marginTop: "1.5rem",
-                }}
-              />
-            </Grid>
-          </Grid>
-        </ThemeProvider>
-      </FormContainer>
-    </Box>
-  );
+    useEffect(() => {
+        const isValid = isChecked && Object.values(bookingForm).every(value => value !== ""); // Ensure all fields are filled and checkbox is checked
+        updateValidation(isValid);
+    }, [formData, isChecked, updateValidation, bookingForm]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setBookingForm({
+            ...bookingForm,
+            [name]: value,
+        });
+
+        form.updateFormData({ [name]: value });
+    };
+
+    const handleCheckboxChange = (e) => {
+        setIsChecked(e.target.checked); // Update checkbox state
+    };
+
+    return (
+        <Box
+            sx={{
+                padding: "2rem 1rem",
+                maxWidth: "800px",
+                margin: "auto",
+                "@media (max-width: 600px)": {
+                    position: "relative",
+                    top: "-3rem",
+                },
+            }}
+        >
+            <FormContainer
+                component="form"
+                sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+            >
+                <ThemeProvider theme={(outerTheme) => deepmerge(outerTheme, theme)}>
+                    {/* Form Fields */}
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="First Name"
+                                name="firstName"
+                                value={bookingForm.firstName}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Surname"
+                                name="surname"
+                                value={bookingForm.surname}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Phone Number"
+                                name="phoneNumber"
+                                value={bookingForm.phoneNumber}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Company Name"
+                                name="companyName"
+                                value={bookingForm.companyName}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Street"
+                                name="street"
+                                value={bookingForm.street}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={6} md={3}>
+                            <CustomFormTextField
+                                label="City"
+                                name="city"
+                                value={bookingForm.city}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={6} md={3}>
+                            <CustomFormTextField
+                                label="Zip Code"
+                                name="zipCode"
+                                value={bookingForm.zipCode}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Email"
+                                name="email"
+                                value={bookingForm.email}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <CustomFormTextField
+                                label="Make and Model of Vehicle"
+                                name="makeModel"
+                                value={bookingForm.makeModel}
+                                onChange={handleChange}
+                                fullWidth
+                                sx={{
+                                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+                                    borderRadius: "8px",
+                                    marginTop: "1.5rem",
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Box sx={{ marginTop: "1.5rem" }}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={isChecked}
+                                    onChange={handleCheckboxChange}
+                                    color="primary"
+                                />
+                            }
+                            label={
+                                <Typography variant="body2">
+                                    I certify that I have read and agree to the{" "}
+                                    <Typography component="span" fontWeight="bold">
+                                        Terms and Conditions
+                                    </Typography>
+                                </Typography>
+                            }
+                        />
+                    </Box>
+                </ThemeProvider>
+            </FormContainer>
+        </Box>
+    );
 };
 
 export default BookingParticulars;
