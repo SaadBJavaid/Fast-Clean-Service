02f2dce76bcefa2b4c8fec6fc5aef1781cd01f5f
Faizan Haq <fizoneechan@gmail.com>
2024-11-05 07:51:16 +0500

    make smallscreenview default

--- src/components/BookingForm/ScheduleAppointmentStep/index.jsx
+++ src/components/BookingForm/ScheduleAppointmentStep/index.jsx
@@ -1,18 +1,22 @@
 import {Box} from "@mui/material";
 import React from "react";
-import ScheduleAppointment from "./ScheduleAppointment";
 import {BookingFormHeading, BookingFormSubHeading } from "../../mui/BookingFormPackages";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import SmallScreenView from "./smallScreenView";
 
 const Index = () => {
   return (
     <Box>
       <BookingFormHeading>Schedule Appointment</BookingFormHeading>
-        <BookingFormSubHeading>
-            Please select an appointment time <br/>
-            We’re ready to clean when it’s most convenient for you!
-        </BookingFormSubHeading>
+      <BookingFormSubHeading>
+        Please select an appointment time <br />
+        We’re ready to clean when it’s most convenient for you!
+      </BookingFormSubHeading>
       <Box>
-        <ScheduleAppointment />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <SmallScreenView />
+        </LocalizationProvider>
       </Box>
     </Box>
   );
